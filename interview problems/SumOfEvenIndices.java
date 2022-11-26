74eead55f87682c0f4c932c95bfd2b5213da72af
Navaneet514 <90147074+Navaneet514@users.noreply.github.com>
2022-11-26 10:52:12 +0530

    Create SumOfEvenIndices.java
    You are given an array A of length N and Q queries given by the 2D array B of size Q*2. Each query consists of two integers B[i][0] and B[i][1].
    For every query, the task is to calculate the sum of all even indices in the range A[B[i][0]…B[i][1]].

---
+++ interview problems/SumOfEvenIndices.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B) {
+
+        ArrayList<Integer> PFE = new ArrayList<Integer>();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+
+        PFE.add(A.get(0));
+
+        for (int i = 1; i < A.size(); i++) {
+            if (i%2 == 1) PFE.add(PFE.get(i-1));
+            else PFE.add(PFE.get(i-1) + A.get(i));            
+        }
+
+        for(int i = 0; i < B.size(); i++) {
+            int start = B.get(i).get(0);
+            int end = B.get(i).get(1);
+
+            if (start == 0) {
+                ans.add(PFE.get(end));
+            }
+            else {
+                ans.add(PFE.get(end) - PFE.get(start-1));
+            }
+        }
+        return ans;
+    }
+}
